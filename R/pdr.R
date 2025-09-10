c04aa2a3ea01c5682e7017982c99734578a1471e
deepchocolate <andreas@systerdyster.se>
2025-09-10 16:29:33 +0200

    Fixed checks

--- R/pdr.R
+++ R/pdr.R
@@ -1,4 +1,9 @@
 #' Perform QC on the drug register
+#' @param fileInput The drug register.
+#' @param fileOutput The output file.
+#' @param formatInput File format of input.
+#' @param formatOutput File format of output.
+#' @importFrom utils read.csv write.csv
 processDrugRegister <- function(fileInput, fileOutput, formatInput='csv', formatOutput='csv') {
   dtaIn <- read.csv(fileInput)
   nr <- nrow(dtaIn)
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,9 +2,9 @@
 #' @param error Any value in the column ERROR that should indicate an ERROR
 #' @return The temporary filename
 dataPDRCreate <- function (error=F) {
-  dta <- read.csv('data/norwegian_drug_register.csv')
+  dta <- utils::read.csv('data/norwegian_drug_register.csv')
   fileNew <- withr::local_tempfile(.local_envir = .GlobalEnv)
   if (!isFALSE(error)) dta <- subset(dta, ERROR %in% error)
-  write.csv(dta, file=fileNew)
+  utils::write.csv(dta, file=fileNew)
   fileNew
 }
