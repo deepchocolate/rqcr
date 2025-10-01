90aa41da08cf3bf4c27eca3d65b2d529b288487f
deepchocolate <andreas@systerdyster.se>
2025-10-01 16:39:11 +0200

    Export of function and documentation

--- R/data-conversion.R
+++ R/data-conversion.R
@@ -1,5 +1,12 @@
 #' @importFrom glue glue
 NULL
+#' Convert a database table into a parquet file.
+#' @export
+#' @param dbCon A database connection (needs to support conversion to parquet).
+#' @param table Database table name.
+#' @param pathOutput Parquet output path.
+#' @param partition Whether to partition data overa column.
+#' @param overwrite Whether to overwrite an existing parquet file.
 convertDBTableToParquet <- function (dbCon, table, pathOutput, partition=F, overwrite=T) {
   q <- glue('COPY {table} TO "{pathOutput}" (FORMAT "parquet", OVERWRITE {overwrite}, COMPRESSION zstd')
   if (!isFALSE(partition)) q <- glue(q, ', PARTITION_BY {partition}')
