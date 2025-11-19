632c5450b2d5e1528ccb4b21fe94a726b2c19bf7
deepchocolate <andreas@systerdyster.se>
2025-11-19 11:02:18 +0100

    Reorganized, added function

---
+++ R/utils-stats.R
@@ -0,0 +1,51 @@
+#' Count frequencies of unique values
+#' @import dplyr
+#' @export
+#' @details
+#' Without any columns passed in (...) this function simply returns the distinct
+#' values, counts and percent of these values. This behaviour is the same as `unique`,
+#' with a count for each value and a parecent. However, by passing other
+#' columns through `...` the function performs this operation over these columns so
+#' that each value is only counted once for each group defined as the unique combination
+#' of values in these columns.
+#' @seealso [frequencyCountDiscrete()]
+#' @param dta Any data accepted by dplyr.
+#' @param col Column in dta to count values.
+#' @param ... Columns to select in data.
+#' @return A tibble with columns in `col`, and and columns `N` four counts
+#' and `Percent` for percentage of values.
+frequencyCountDistinct <- function (dta, col, ...) {
+  dta <- dta %>% select(..., all_of(col)) %>% distinct() %>% select(all_of(col)) %>% collect() %>% count(pick(all_of(col)), name='n') %>%
+    arrange(!!as.name(col))
+  colnames(dta) <- c(col, 'n')
+  n <- sum(dta$n)
+  dta$Percent <-  100*dta$n/n
+  colnames(dta) <- c(col, 'N', 'Percent')
+  dta
+}
+
+#' Count frequencies of discrete values
+#' @details
+#' Counting is performed by counting rows in data which forms combinations
+#' of unique values by the specified columns in `...`. This is what `dplyr::count`
+#' does, but adds a percentage column.
+#'
+#' @export
+#' @import dplyr
+#' @param .data Any data accepted by dplyr.
+#' @param ... Column(s) to form discrete values in `dta`.
+#' @return A tibble with columns in `...` and columns for counts (`N`) and percent.
+frequencyCountDiscrete <- function (.data, ...) {
+  .data %>% select(...) %>% collect() %>% count(..., name='N') %>%
+    percent()
+}
+
+#' Calculate percent based on counts
+#' @export
+#' @param .data Anything accepted by dplyr.
+#' @param .colCount Name of column with counts.
+#' @param ... Grouping variables.
+percent <- function (.data, .colCount='N', ...) {
+  .data %>% group_by(...) %>% mutate(Percent=100*!!as.name(.colCount)/sum(!!as.name(.colCount))) %>%
+    ungroup()
+}
---
+++ tests/testthat/test_utils-stats.R
@@ -0,0 +1,32 @@
+test_that('Test frequencyCount', {
+  fileIn <- dataPDRCreate()
+  dta <- arrow::open_dataset(fileIn %s+% '.parquet')
+  tmp <- frequencyCountDistinct(dta, 'ATC')
+  expect_equal(tmp, tibble(ATC=c('N06BA01', 'N06BA02'), N=c(1,1), Percent=c(50,50)))
+  tmp <- frequencyCountDiscrete(dta, ATC)
+  expect_equal(tmp, tibble(ATC=c('N06BA01', 'N06BA02'), N=c(3,1), Percent=c(75,25)))
+  tmp <- dta %>% frequencyCountDiscrete(ATC)
+  expect_equal(tmp, tibble(ATC=c('N06BA01', 'N06BA02'), N=c(3,1), Percent=c(75,25)))
+  # This will just be a simple rowcount
+  expect_equal(frequencyCountDiscrete(dta), tibble(N=4, Percent=100))
+  # Count over PIN
+  tmp <- frequencyCountDistinct(dta, 'ATC', 'lopenr')
+  expDta <- data.frame(ATC=c('N06BA01', 'N06BA02'), N=c(2, 1), Percent=c(100*2/3, 100/3))
+  expect_equal(as.data.frame(tmp), expDta)
+  tmp <- frequencyCountDiscrete(dta, lopenr, ATC)
+  expect_equal(tmp, tibble(lopenr=c('I1','I2','I3'), ATC=c('N06BA01','N06BA02','N06BA01'), N=c(1,1,2), Percent=c(25,25,50)))
+  # Counts over two distinct values
+  tmp <- frequencyCountDistinct(dta, c('ATC', 'Date'), 'lopenr')
+  expDta <- data.frame(
+    ATC=c(rep('N06BA01', 3), 'N06BA02'),
+    Date=c('2024-12-31','2025-01-03', 'x','2025-01-03'),
+    N=rep(1,4),
+    Percent=rep(25,4)
+  )
+  expect_equal(as.data.frame(tmp), expDta)
+  tmp <- frequencyCountDiscrete(dta, ATC, Date, lopenr)
+  expDta <- collect(dta)[, c('ATC', 'Date', 'lopenr')]
+  expDta$N <- 1
+  expDta$Percent <- 25
+  expect_equal(tmp, expDta %>% arrange(ATC))
+})
