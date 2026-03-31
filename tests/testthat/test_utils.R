680a6b4237c987f3f36adf5ed73705b073f2135e
deepchocolate <andreas@systerdyster.se>
2026-03-31 22:53:11 +0200

    Added tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -219,4 +219,5 @@ test_that('txtNPercent', {
   expect_equal(txtNPercent(0, 0), '0 (0)')
   configRQCR('txtNPercent', 'maxPercent', 100)
   expect_warning(txtNPercent(10, 1))
+  expect_equal(txtNPercent(10, 1, maxPercent=1000), '10 (1000)')
 })
